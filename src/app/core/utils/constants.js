d7071047d3bb7e758992f62cf847b6c549239e79
Jarauta1 <diegojarauta8@gmail.com>
2021-12-18 01:14:16 +0100

    players service

---
+++ src/app/core/utils/constants.js
@@ -0,0 +1,9 @@
+export class CONSTANTS {
+
+    //JUGADOR
+    static GET_JUGADORES = '/jugador/get-all';
+    static GET_JUGADOR_ID = '/jugador/by-id/:id';
+    static CREATE_JUGADOR = '/jugador/create';
+    static UPDATE_JUGADOR = '/jugador/update/:id';
+    static DELETE_JUGADOR = '/jugador/delete/:id';
+}
