1adf1349caef11e4c99b235ecc9d9f3df413a9bd
Jarauta1 <diegojarauta8@gmail.com>
2021-12-18 01:13:26 +0100

    start weather service

---
+++ src/app/core/services/weather.service.js
@@ -0,0 +1,13 @@
+import { environment } from "../../../environments/environments";
+import { CONSTANTS } from "../utils/constants";
+
+export class WeatherService {
+
+    constructor (
+        /* private readonly httpClient: HttpClient */
+    ){}
+
+    getWeather() {
+       
+    }
+}
