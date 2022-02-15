e1bac0bd360c6d6ca5f862fc664f147796b059fd
Jarauta1 <diegojarauta8@gmail.com>
2022-02-16 00:17:11 +0100

    player screen

--- src/app/components/jugadores/jugadores.component.js
+++ src/app/components/jugadores/jugadores.component.js
@@ -1,101 +1,31 @@
-import { useState, useEffect, Component } from "react";
-import JugadorService from "../../core/services/jugador.service";
+import { useEffect, useState } from "react";
+import jugadorService from "../../core/services/jugador.service";
+import Card from "../../containers/playercard/player.card";
+import "./jugadores.component.css";
 
-function Jugadores(props) {
+function Players(props) {
 
-
-    /*  let [jugadores, setJugadores] = useState([]) */
+    const [players, setPlayers] = useState([])
 
     useEffect(function () {
         getJugadores();
     }, [])
 
     function getJugadores() {
-        JugadorService.getJugadores().then((data) => {
-            console.log(data)
+        jugadorService.getJugadores().then((data) => {
+            setPlayers(data)
         })
     }
 
-    function getJugadorById(id) {
-        JugadorService.getJugadorById(id).then((data) => {
-            console.log(data)
-        })
-    }
+    const showPlayers = players.map( player => {
+        return <Card jugador={player}/>
+    })
 
-    function createJugador(jugador) {
-        JugadorService.createJugador(jugador).then((data) => {
-            console.log(data)
-        })
-    }
-
-    function updateJugador(jugador, id) {
-        JugadorService.updateJugador(jugador, id).then((data) => {
-            console.log(data)
-        })
-    }
-
-    function deleteJugador(id) {
-        JugadorService.deleteJugador(id).then((data) => {
-            console.log(data)
-        })
-    }
-
-
-    return (<div>
-        <button onClick={getJugadores} className="btn btn-success">
-            TODOS
-        </button>
-        <button onClick={getJugadorById(1)} className="btn btn-success">
-            UNO
-        </button>
-        <button onClick={createJugador({
-            equipo_id: 1,
-            nombre_jugador: "Pablo",
-            apellido1: "Perez",
-            apellido2: "Muñoz",
-            apodo: "Ariete",
-            fecha_nacimiento: "1987-08-23",
-            foto_jugador: "",
-            dorsal: 41,
-            posicion: "Pivot",
-            altura: 180,
-            total_goles: 2,
-            total_asistencias: 0,
-            total_amarillas: 0,
-            total_rojas: 0,
-            total_encajados: 0,
-            pais: "España",
-            ciudad: "Zaragoza"
-        })} className="btn btn-success">
-            CREAR
-        </button>
-        <button onClick={updateJugador({
-            jugador_id: 2,
-            equipo_id: 1,
-            nombre_jugador: "Pablo",
-            apellido1: "Perez",
-            apellido2: "Muñoz",
-            apodo: "Ariete",
-            fecha_nacimiento: "1987-08-23",
-            foto_jugador: "",
-            dorsal: 41,
-            altura: 180,
-            total_goles: 4,
-            total_asistencias: 0,
-            total_amarillas: 0,
-            total_rojas: 0,
-            total_encajados: 0,
-            pais: "España",
-            ciudad: "Zaragoza"
-        },2)} className="btn btn-success">
-            ACTUALIZAR
-        </button>
-        <button onClick={deleteJugador(2)} className="btn btn-success">
-            BORRAR
-        </button>
+    return(<div className="screen">
+        <div className="player-card">
+            { showPlayers }
+        </div>
     </div>)
-
-
 }
 
-export default Jugadores;
+export default Players;
