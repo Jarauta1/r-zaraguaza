f2368c29f147bef329a24ad32df3c8998655c45c
Jarauta1 <diegojarauta8@gmail.com>
2022-02-16 00:16:33 +0100

    Main routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './app/components/header/header';
-import Jugadores from './app/components/jugadores/jugadores.component';
+/* import Classification from './app/components/classification/classification.component'; */
+import Players from './app/components/jugadores/jugadores.component';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header/>
-        <Jugadores/>
+        <div className='screen'>
+          <Route exact path="/">
+
+          </Route>
+         {/*  <Route exact path="/classification">
+            <Classification />
+          </Route> */}
+          <Route exact path="/players">
+            <Players />
+          </Route>
+        </div>
       </BrowserRouter>
     </div>
   );
